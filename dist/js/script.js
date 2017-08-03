cb96b9522c6e6fa9d44662ec191364599175beb6
Unknown <dr.fender17@gmail.com>
2017-08-03 16:38:25 +0300

    add  functionality

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,13 @@
 var app = angular.module('myApp', []);
 app.controller('firstCtrl', function ($scope){
-	$scope.comment = [];
+	$scope.comment = [1,2,3];
 	$scope.addItem = function () {
 		if (!$scope.Item) {
 			return
 		}
 		if ($scope.comment.indexOf($scope.Item) == -1) {
 			$scope.comment.push($scope.Item);
+			$scope.Item = "";
 		}
 		else {
 			return
